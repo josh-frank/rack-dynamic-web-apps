9b476fb1cc4b74523ca1f8a1b5343ba857fb550a
Josh Frank <eurosewusa@gmail.com>
2021-01-08 17:14:08 -0500

    commit tree 5cb413ef07323143a372459c9b4775df0fb785a3

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,17 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    one = Kernel.rand( 1..7 )
+    two = Kernel.rand( 1..7 )
+    three = Kernel.rand( 1..7 )
+    resp.write "#{ one }\n"
+    resp.write "#{ two }\n"
+    resp.write "#{ three }\n"
+    if one == two && two == three
+      resp.write "You Win"
+    else
+      resp.write "You Lose"  
+    end
     resp.finish
   end
 
